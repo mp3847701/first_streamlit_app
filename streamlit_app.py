cf1f1ab1992f3967f65e6e59b090b963a484c912
mp3847701 <129932777+mp3847701@users.noreply.github.com>
2023-04-08 13:01:45 +0200

    Update streamlit_app.py
     translate json format to table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# translate json format to table format 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#Display the table on the page.
+streamlit.dataframe(fruityvice_normalized)
